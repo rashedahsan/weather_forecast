18bd03601058cfb86848830eccf493cf380b1a2a
rashedahsan <polashmondol88@gmail.com>
2024-09-28 21:56:32 +0600

    api model

---
+++ lib/constants/constants.dart
@@ -0,0 +1,2 @@
+const base_url =
+    "http://api.weatherapi.com/v1/forecast.json?key=e66061902e64414c980172933242709&q=London&days=7";
---
+++ lib/service/api_service.dart
@@ -0,0 +1,16 @@
+// import 'package:http/http.dart';
+// import 'package:weather_app/constants/constants.dart';
+// import 'package:weather_app/model/weather_model.dart';
+
+// class ApiService {
+//   Future<weathermodel> getWeatherData(String searchText) async {
+//     String url = "$base_url&q=searchText&days=7";
+
+//     try {
+//       Response response = await get(Uri.parse(url));
+//       if (response.statusCode == 200) ; 
+//     } catch (e) {
+//       throw e.toString();
+//     }
+//   }
+// }
